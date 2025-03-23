8140530e309ac0dd633658da1e23b62bbf8dce39
Reese408 <reeseredman099@gmail.com>
2025-03-23 19:06:50 -0400

    Delete website directory

--- website/routes/storeRoutes.js
+++
@@ -1,8 +0,0 @@
-// routes/storeRoutes.js
-const express = require('express');
-const router = express.Router();
-const storeController = require('../controllers/storeController');
-
-router.get('/store', storeController.getStoreItems);
-
-module.exports = router;
